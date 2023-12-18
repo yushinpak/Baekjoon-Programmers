87aee19a31678af5f949eab5d24e2787cb2a1ccb
chacha <153361838+yushinpak@users.noreply.github.com>
2023-12-18 23:57:58 +0900

    [Bronze V] Title: Hello World, Time: 132 ms, Memory: 9420 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.js
@@ -0,0 +1,15 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+
+    console.log("Hello World!")
+    
+    process.exit();
+});
