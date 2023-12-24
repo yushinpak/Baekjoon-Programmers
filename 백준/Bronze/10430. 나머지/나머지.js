f95ad807b3f441c844f87a1631259254d11463a0
chacha <153361838+yushinpak@users.noreply.github.com>
2023-12-24 10:05:31 +0900

    [Bronze V] Title: 나머지, Time: 148 ms, Memory: 9712 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,22 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){
+	
+    let A = input[0]
+    let B = input[1]
+    let C = input[2]
+        
+        console.log((A+B)%C)
+        console.log(((A%C) + (B%C))%C,)
+        console.log((A*B)%C)
+        console.log(((A%C) * (B%C))%C)
+    
+    process.exit();
+});
