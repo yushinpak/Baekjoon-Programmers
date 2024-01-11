1a1a6a54af9d800be0afd111e34ec9e90337fb1d
chacha <153361838+yushinpak@users.noreply.github.com>
2024-01-11 13:25:16 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,17 +1,21 @@
+// 남의 풀이
 function solution(n) {
     var answer = 0;
-    
-    if (n % 2 === 1){
-        for (let i = 0; i < n/2; i++){
-            answer += 1+2*i//홀수인 모든 양의 정수의 합
-        } 
-    } 
-    
-    else {
-        for (let i = 1; i <= n/2; i++){
-            answer += (2*i)*(2*i)//짝수인 모든 양의 정수의 제곱의 합
+
+    if(n%2==0){
+        for(let i=1; i<=n; i++){
+            if(i%2==0){
+                answer += i*i;
+            }
         }
     }
-    
+    else{
+        for(let i=1; i<=n; i++){
+            if(i%2==1){
+                answer += i;
+            }
+        }
+    }
+
     return answer;
 }
