05de5ef42b44fbf181e2b8c80ebe09e9804ed646
chacha <153361838+yushinpak@users.noreply.github.com>
2024-01-31 13:05:07 +0900

    Update 단어의 개수.js

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -8,6 +8,7 @@ const input = require('fs')
     .trim()
     .split(' ');
 
+//시도한 풀이 if문 / 삼항연산자 사용 / for문으로 세어보기 / forEach문으로 세어보기
 
 //첫번째 시도한 풀이(맨첫문자 ''경우 제거)
 // let answer = input.length
@@ -20,5 +21,27 @@ const input = require('fs')
 
 //2번 - 삼항연산자 사용해보기
 
+// input[0] === ""
+//     ? console.log(input.length - 1)
+//     : console.log(input.length)
 
-input[0] === "" ? console.log(input.length - 1) : console.log(input.length)
+//3번 - for문으로 세어보기
+
+// let answer_num = 0;
+
+// for(let i = 0; i < input.length;  i++){
+//     if (input[i] !== ''){
+//     answer_num++
+//     }
+// }
+
+// console.log(answer_num)
+
+//마지막 - forEach 문으로 바꿔보기
+let answer_num = 0;
+input.forEach( (el) => {
+    if (el !== ''){
+    answer_num++
+    }
+})
+console.log(answer_num);
