1dbc69cb325e9b45b9e8a89fc76381c31a8904d4
yushin pak <153361838+yushinpak@users.noreply.github.com>
2024-05-16 07:59:01 +0900

    Update 크로아티아 알파벳.js
    보충 공부

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -1,45 +1,69 @@
 const input = require("fs")
-  // .readFileSync(process.platform === "linux" ? "/dev/stdin" : "2941/input.txt")
-  // .readFileSync(process.platform === "linux" ? "/dev/stdin" : "2941/input2.txt")
-  // .readFileSync(process.platform === "linux" ? "/dev/stdin" : "2941/input3.txt")
-  // .readFileSync(process.platform === "linux" ? "/dev/stdin" : "2941/input4.txt")
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "2941/input5.txt")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "2941/input.txt")
   .toString()
   .trim();
 
+// 내 풀이 - 거의 하드코딩. 이외의 방법 생각 못함
+// 내가 몰랐던 것 - (1) replaceAll 메서드 (2) for of문 
 let answer = input.length;
-// console.log("원래 글자 수", answer);
 
 for (let i = 0; i < input.length; i++) {
   if (input[i] === "d") {
     if (input[i + 1] === "z" && input[i + 2] === "=") {
       answer -= 2;
-      //   console.log("dz=");
     } else if (input[i + 1] === "-") {
       answer -= 1;
-      //   console.log("d-");
     }
   } else if (input[i] === "c") {
     if (input[i + 1] === "=") {
       answer -= 1;
-      //   console.log("c=");
     } else if (input[i + 1] === "-") {
       answer -= 1;
-      //   console.log("c-");
     }
   } else if (input[i] === "l" && input[i + 1] === "j") {
     answer -= 1;
-    // console.log("lj");
   } else if (input[i] === "n" && input[i + 1] === "j") {
     answer -= 1;
-    // console.log("nj");
   } else if (input[i] === "s" && input[i + 1] === "=") {
     answer -= 1;
-    // console.log("s=");
   } else if (input[i] === "z" && input[i + 1] === "=" && input[i - 1] !== "d") {
     answer -= 1;
-    // console.log("z=");
   }
 }
 
 console.log(answer);
+
+// 다른 풀이 - reduce, replaceAll 사용
+console.log('c= c- dz= d- lj nj s= z='.split(' ').reduce((a,v,i)=>a.replaceAll(v,i),(require('fs').readFileSync(0)+"").trim()).length)
+
+// reduce - 배열의 각 요소를 순회하며 callback 함수의 실행 값을 누적하여 하나의 결과값을 반환
+// 인수 - accumulator(callback 함수의 반환값을 누적), currentValue, index, array
+// replaceAll - pattern의 모든 일치 항목이 replacement로 대체된 새 문자열을 반환
+// 인수 - pattern, replacement
+
+// 다른 풀이 - for of문 사용
+// 특히     words = words.split(i).join("A"); -> 이렇게 하면 '대체'와 똑같은 결과 낼 수 있음!
+
+const fs = require('fs'); 
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+const croatia = [ "c=" , "c-" , "dz=" , "d-", "lj", "nj", "s=", "z="];
+let words = input;
+
+for (let i of croatia) {
+    words = words.split(i).join("A");
+}
+
+console.log(words.length);
+
+// 다른 풀이 - replaceAll 사용
+const input = require('fs').readFileSync('/dev/stdin').toString().trim()
+
+const alphabet = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+
+let modifiedWrod = input;
+alphabet.forEach(alpha => {
+    modifiedWrod = modifiedWrod.replaceAll(alpha, 'X')
+})
+
+console.log(modifiedWrod.length)
