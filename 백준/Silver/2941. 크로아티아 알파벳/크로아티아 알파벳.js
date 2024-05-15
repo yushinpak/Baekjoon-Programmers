9a239f9d06f061a13cf977a5295cf839d6eea1da
yushin pak <153361838+yushinpak@users.noreply.github.com>
2024-05-16 08:00:00 +0900

    보충 공부

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -4,7 +4,16 @@ const input = require("fs")
   .trim();
 
 // 내 풀이 - 거의 하드코딩. 이외의 방법 생각 못함
+
 // 내가 몰랐던 것 - (1) replaceAll 메서드 (2) for of문 
+// reduce - 배열의 각 요소를 순회하며 callback 함수의 실행 값을 누적하여 하나의 결과값을 반환
+// 인수 - accumulator(callback 함수의 반환값을 누적), currentValue, index, array
+// replaceAll - pattern의 모든 일치 항목이 replacement로 대체된 새 문자열을 반환
+// 인수 - pattern, replacement
+
+// 특히     words = words.split(i).join("A"); -> 이렇게 하면 '대체'와 똑같은 결과 낼 수 있음!
+
+
 let answer = input.length;
 
 for (let i = 0; i < input.length; i++) {
@@ -36,13 +45,7 @@ console.log(answer);
 // 다른 풀이 - reduce, replaceAll 사용
 console.log('c= c- dz= d- lj nj s= z='.split(' ').reduce((a,v,i)=>a.replaceAll(v,i),(require('fs').readFileSync(0)+"").trim()).length)
 
-// reduce - 배열의 각 요소를 순회하며 callback 함수의 실행 값을 누적하여 하나의 결과값을 반환
-// 인수 - accumulator(callback 함수의 반환값을 누적), currentValue, index, array
-// replaceAll - pattern의 모든 일치 항목이 replacement로 대체된 새 문자열을 반환
-// 인수 - pattern, replacement
-
 // 다른 풀이 - for of문 사용
-// 특히     words = words.split(i).join("A"); -> 이렇게 하면 '대체'와 똑같은 결과 낼 수 있음!
 
 const fs = require('fs'); 
 const input = fs.readFileSync("/dev/stdin").toString().trim();
