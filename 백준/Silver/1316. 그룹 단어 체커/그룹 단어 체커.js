ccf1dffcd8b8e093823eb496dcae3ddec5e58e14
yushin pak <153361838+yushinpak@users.noreply.github.com>
2024-05-16 17:36:13 +0900

    다른 풀이 공부

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -4,6 +4,12 @@ const input = require("fs")
   .trim()
   .split("\n");
 
+
+// 내 풀이 - 중복값이 없는 배열을 만들고 수를 세어 중복값이 없으면 통과 || 마지막 인덱스 위치 - 첫 인덱스 + 1 = 등장한 해당 알파벳의 개수면 통과
+// 새로 배운 것 - set 객체로 중복값을 제거한 배열 만들 수 있음, forEach로 중복값을 세는 객체 쉽게 만들 수 있다
+
+// 다른 풀이 - '이전에 등장한 알파벳'을 쉽게 알아보는 방법은 indexOf -> 이런 방식이 일반적인 풀이임. 내가 indexOf를 떠올리지 못해서 조금 돌아간 것 같다!
+
 const array = input.slice(1); // 단어만 배열로 만든 것
 let answer = 0;
 
@@ -25,7 +31,7 @@ array.forEach((word) => {
     const lastChar = wordArray.lastIndexOf(pureArray[i]);
     const charNum = result[pureArray[i]];
 
-    // 중복값이 없으면 통과 or 마지막 인덱스 위치 - 첫 인덱스 +1 = 0이면 통과
+    // 중복값이 없으면 통과 or 마지막 인덱스 위치 - 첫 인덱스 +1 = 알파벳의 개수이면 통과
     if (
       wordArray.length === pureArray.length ||
       lastChar - firstChar + 1 === charNum
