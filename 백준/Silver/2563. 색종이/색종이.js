b7052c0124e5d6c3e6e142fe18227f75fd9399d8
yushin pak <153361838+yushinpak@users.noreply.github.com>
2024-05-30 20:28:01 +0900

    [Silver V] Title: 색종이, Time: 256 ms, Memory: 10848 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.js
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -1,27 +1,22 @@
-const fs = require("fs");
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "2563/input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .slice(1);
 
-const readInput = (filePath) => {
-  return fs.readFileSync(filePath).toString().trim().split("\n").slice(1);
-};
+let paintedCell = [];
 
-const createAnswer = (input) => {
-  let paintedCell = [];
-  input.forEach((el) => {
-    const [startingPoint_X, startingPoint_Y] = el.split(" ").map(Number);
-    for (let x = startingPoint_X; x < startingPoint_X + 10; x++) {
-      for (let y = startingPoint_Y; y < startingPoint_Y + 10; y++) {
-        const coordinate = `${x},${y}`;
-        if (paintedCell.indexOf(coordinate) === -1) {
-          paintedCell.push(coordinate);
-        }
+input.forEach((el) => {
+  const [startingPoint_X, startingPoint_Y] = el.split(" ").map(Number);
+  for (let x = startingPoint_X; x < startingPoint_X + 10; x++) {
+    for (let y = startingPoint_Y; y < startingPoint_Y + 10; y++) {
+      const coordinate = `${x},${y}`;
+      if (paintedCell.indexOf(coordinate) === -1) {
+        paintedCell.push(coordinate);
       }
     }
-  });
-  return (paintedCell.length)
-};
+  }
+});
 
-const input = readInput(
-  process.platform === "linux" ? "/dev/stdin" : "2563/input.txt"
-);
-const answer = createAnswer(input);
-console.log(answer)
+console.log(paintedCell.length);
